a02e6bf1b2fd5b24770e0a439ecde11a9c286855
Geoff Lewis <geoff@geofflewis.work>
2018-03-06 13:43:15 -0700

    Corrected js file path and added event listener

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,3 +1,8 @@
 const startScreen = document.getElementById('overlay');
+const startButton = document.getElementsByClassName('btn__reset')[0];
 const qwertyKeyboard = document.getElementById('qwerty');
 const phrase = document.getElementById('phrase');
+
+startButton.addEventListener('click', () => {
+     startScreen.style.display = 'none';
+})
