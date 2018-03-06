ec426370d9b35cc1b8438b8be3f0f41d5d30f51d
Geoff Lewis <geoff@geofflewis.work>
2018-03-06 15:38:12 -0700

    Added variable and comment

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,8 +1,11 @@
+// Variables
 const startScreen = document.getElementById('overlay');
 const startButton = document.getElementsByClassName('btn__reset')[0];
 const qwertyKeyboard = document.getElementById('qwerty');
 const phrase = document.getElementById('phrase');
+let missed = 0;
 
+// Hides start screen overlay when start button is clicked
 startButton.addEventListener('click', () => {
      startScreen.style.display = 'none';
 })
