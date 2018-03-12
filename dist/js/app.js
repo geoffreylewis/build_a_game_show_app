ee1a73ea767a61d88619225bda8e72645b497bf3
Geoff Lewis <geoff@geofflewis.work>
2018-03-12 08:57:59 -0700

    Added function for randomly selecting a phrase

--- dist/js/app.js
+++ dist/js/app.js
@@ -5,10 +5,12 @@ const qwertyKeyboard = document.getElementById('qwerty');
 const phrase = document.getElementById('phrase');
 let missed = 0;
 
+
 // Hides start screen overlay when start button is clicked
 startButton.addEventListener('click', () => {
      startScreen.style.display = 'none';
 })
+
 
 //  Phrases to guess
 const phrases = ['A state of society where men may not speak their minds cannot long endure',
@@ -16,3 +18,13 @@ const phrases = ['A state of society where men may not speak their minds cannot 
                  'History is written by the victors',
                  'Great and good are seldom the same man',
                  'Difficulties mastered are opportunities won'];
+
+
+// Randomly choose any phrase and split it
+getRandomPhraseAsArray(arr) {
+     let randomPhrase = arr[Math.floor(Math.random()*arr.length)];
+     let randomPhraseArray = specificPhrase.split("");
+     return randomPhraseArray;
+}
+
+getRandomPhraseAsArray(phrases);
