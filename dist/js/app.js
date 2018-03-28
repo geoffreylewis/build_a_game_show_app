0145c3f7690a3a34e3c0687df90fead559f15bf5
Geoff Lewis <geoff@geofflewis.work>
2018-03-28 08:03:30 -0700

    Added another function and event handler

--- dist/js/app.js
+++ dist/js/app.js
@@ -9,6 +9,7 @@ const ul = document.querySelector('ul');
 let missed = 0;
 
 
+
 /*******************
 * Phrases to guess *
 *******************/
@@ -17,6 +18,7 @@ const phrases = ['A state of society where men may not speak their minds cannot 
                  'History is written by the victors',
                  'Great and good are seldom the same man',
                  'Difficulties mastered are opportunities won'];
+
 
 
 /************
@@ -41,15 +43,38 @@ function addPhraseToDisplay(arr) {
      }
 }
 
+//
+function checkLetter (buttonClicked) {
+     let squaresWithLetters = document.getElementsByClassName('letter');
+     let matchingLetter = null;
+     for (let i = 0; i < squaresWithLetters.length; i += 1) {
+          if (squaresWithLetters[i].textContent === buttonClicked.textContent) {
+               squaresWithLetters[i].className += ' show';
+               let matchingLetter = buttonClicked.textContent;
+          }
+     }
+     return matchingLetter;
+}
+
+
 
 /**************
 * Actual Game *
 **************/
-// Hides start screen overlay when start button is clicked
+// Hides start screen overlay when player clicks the start button
 startButton.addEventListener('click', () => {
      startScreen.style.display = 'none';
 });
 
-// Call functions
+// Randomly choose a phrase and put it on the board as empty boxes
 const phraseArray = getRandomPhraseAsArray(phrases);
 addPhraseToDisplay(phraseArray);
+
+// Pays attention to the letters that are clicked on the keyboard by the player
+qwertyKeyboard.addEventListener('click', (e) => {
+     if (e.target.tagName === 'BUTTON') {
+          e.target.className += ' chosen';
+          e.target.disabled = 'true';
+          checkLetter(e.target);
+     }
+})
